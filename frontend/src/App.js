44e27518d610ad363015d537cfffa48fbdc824a8
kid-three <kinich@kbdreams.com>
2021-05-25 17:48:06 -0500

    rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
-      <h1>welcome</h1>
+      <Header />
+      <Container>
+        <main className='py-3'>
+          <HomeScreen />
+        </main>
+      </Container>
+      <Footer />
     </>
   )
 }
